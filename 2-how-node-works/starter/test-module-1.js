5600a4afcab0c1e1ff1e59f7210290a79b0d1a3e
Clive Broom <clivebroom@gmail.com>
2024-12-13 20:29:55 +0000

    all

---
+++ 2-how-node-works/starter/test-module-1.js
@@ -0,0 +1,28 @@
+//Option1
+
+/* class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+  divide(a, b) {
+    return a / b;
+  }
+} */
+
+//module.exports = Calculator;
+
+//option 2
+module.exports = class {
+  add(a, b) {
+    return a + b;
+  }
+  multiply(a, b) {
+    return a * b;
+  }
+  divide(a, b) {
+    return a / b;
+  }
+};
