eac0119e89bd59bca9463b66c2a9ccf12f1007be
Clive Broom <clivebroom@gmail.com>
2024-12-16 19:32:27 +0000

    a

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   //send text from server
   //res.status(200).send('hello from the server side!');
   //send json
   res
     .status(200)
     .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+}); */
 
-app.post('/', (req, res) => {
+/* app.post('/', (req, res) => {
   res.send('You can post to this endpoint...');
-});
+}); */
 
 const port = 3000;
 app.listen(port, () => {
